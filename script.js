8690b7398a48beb1fc944e8ef81011310f1984ca
Glitch (hello-webpage) <none>
2019-02-27 20:48:40 +0000

    🐷🌮 Checkpoint ./script.js:2992411/1440:2993236/172:3209724/16

--- script.js
+++ script.js
@@ -37,21 +37,35 @@ fetch('https://metaphysics-staging.artsy.net', {
 
 var button = document.getElementById("gene-sup");
 var geneResults = document.getElementById("gene-results");
+var geneDescription = document.getElementById("gene-description");
+
 button.onclick = function(){
     var results = randomPicker(allGenes)
     geneResults.innerHTML = "";
   for (var x=0; x<results.length; x++){
     var gene = results[x];
-    geneResults.innerHTML += "<br></br><a href='https://staging.artsy.net/gene/" + gene._id + "' target=\"_blank\">" + gene.name + "</a>"; 
+    geneResults.innerHTML += "<br></br><a data-description='" + gene.description + "' href='https://staging.artsy.net/gene/" + gene._id + "' target=\"_blank\">" + gene.name + "</a>"; 
+    
+    // add event listent to new <a> elements
   }
   
+  var elements = document.getElementsByTagName("a")
+  for (var x=0; x<elements.length; x++) {
+    elements[x].onmouseover = function(event) {
+      var description = event.target.getAttribute("data-description")
+      geneDescription.innerHTML = description
+    }
+    
+    elements[x].onmouseout = function() {
+      geneDescription.innerHTML = "";
+    }
+  }
 }
 
-var geneDescription = document.getElementById("gene-description");
-button.onhover = function() {
-  geneDescription.innerHTML = "";
-  
-}
+// var geneDescription = document.getElementById("gene-description");
+// button.onmouseover = function() {
+//   geneDescription.innerHTML = "i'm hovering";
+// }
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -60,6 +74,3 @@ function getRandomInt(max) {
 function randomPicker(arrayOfSomething){
  return [arrayOfSomething[getRandomInt(arrayOfSomething.length)], arrayOfSomething[getRandomInt(arrayOfSomething.length)], arrayOfSomething[getRandomInt(arrayOfSomething.length)]]  
 }
-
-
-
